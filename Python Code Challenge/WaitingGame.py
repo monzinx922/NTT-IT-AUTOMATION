1bd39160342cb0091bfe2e63c2f3d131b53720c7
Tanee Buateat <cpe_mon@hotmail.com>
2022-12-25 13:23:11 +0700

    Done for waiting game

--- Python Code Challenge/WaitingGame.py
+++ Python Code Challenge/WaitingGame.py
@@ -1,16 +1,34 @@
 from datetime import datetime
 from datetime import timedelta
 
+# This is a function that let user press enter to start and then stop at specific time
+
 def waiting_game():
+    # User press enter to start game
     print("---Please Enter to Begin---")
     input()
-    t = datetime.time(datetime.now())
+    
+    # Get time that user start
+    t = datetime.now()
     print("The current time is", t)
+
+    # Tell user that they need to press enter again at specific time
     print("---Please Enter again after 4 seconds---")
     input()
-    t2 = datetime.time(datetime.now())
-    print("Your time is",timedelta(t2,t))
-    
+
+    # get time that user input 
+    t2 = datetime.now()
+
+    # set target time using timedelta function
+    time_target = t + timedelta(seconds=4)
+
+    # Show result to user on 3 conditions
+    if t2 > time_target:
+        print("You are slow about ",t2 - time_target)
+    elif time_target > t2:
+        print("You are faster about ",time_target - t2)
+    elif t2 == time_target:
+        print("Congratulation!, You won!")
 
 
 waiting_game()
