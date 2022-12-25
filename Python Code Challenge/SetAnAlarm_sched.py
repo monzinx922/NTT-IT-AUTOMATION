bc1f7b961df0795404e127f91f131b5af2289e98
Tanee Buateat <cpe_mon@hotmail.com>
2022-12-25 17:47:40 +0700

    Solution from course

---
+++ Python Code Challenge/SetAnAlarm_sched.py
@@ -0,0 +1,12 @@
+# Solution from course challenge
+
+import sched
+import time
+import winsound as ws
+
+def set_alarm(alarm_time,wav_file,message):
+    s = sched.scheduler(time.time, time.sleep)
+    s.enterabs(alarm_time,1,print,argument=(message,))
+    s.enterabs(alarm_time,1,ws.PlaySound,argument=(wav_file,ws.SND_FILENAME))
+    print('Alarm set for', time.asctime(time.localtime(alarm_time)))
+    s.run()
